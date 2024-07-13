bf1793584c8d6d4d595c3e9a77a75e3c37a25776
mikelangelon <mikelangelon@gmail.com>
2024-07-13 17:06:03 +0200

    Add idle animation

--- graphics/char.go
+++ graphics/char.go
@@ -5,27 +5,30 @@ import (
 	"github.com/mikelangelon/town-sweet-town/common"
 )
 
+const frameSpeed = 20
+
 type Char struct {
-	ID    string
-	Image *ebiten.Image
-	X, Y  int64
+	ID            string
+	Image         *ebiten.Image
+	IdleAnimation []*ebiten.Image
+	X, Y          int64
 
 	ScaleX, ScaleY float64
-	Stuff          []*ebiten.Image
+	frameCount     int
 }
 
 func (c *Char) Position() common.Position {
 	return common.Position{X: c.X, Y: c.Y}
 }
 
+func (c *Char) Update() {
+	c.frameCount++
+}
 func (c *Char) Draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(float64(c.X), float64(c.Y))
 	op.GeoM.Scale(c.ScaleX, c.ScaleY)
 
-	screen.DrawImage(c.Image, op)
-
-	for _, v := range c.Stuff {
-		screen.DrawImage(v, op)
-	}
+	image := c.IdleAnimation[(c.frameCount/frameSpeed)%len(c.IdleAnimation)]
+	screen.DrawImage(image, op)
 }
